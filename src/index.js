17b2c3eeb9402458e4e3375c8c37663c14b3a3db
balint magyar <magyarb0201@gmail.com>
2019-02-16 01:06:00 +0100

    Couple slight improvements

--- src/index.js
+++ src/index.js
@@ -5,62 +5,66 @@ const fs = require('fs')
 const helpers = require('yeoman-test')
 
 module.exports = {
-    initGenerator,
-    cleanupGenerator,
-    file,
-    fileJSON
+  initGenerator,
+  cleanupGenerator,
+  file,
+  fileJSON
 }
 
-let currentGenerator
+let tmpDir
 
 function initGenerator(genPath) {
-    let gen = helpers.run(genPath)
-    
-    return {
-        setupDir: cb => gen.inTmpDir(function (dir) {
-            let done = this.async()
-            let didCallDone = false
-            let doneCb = () => {
-                didCallDone = true
-                done()
-            }
-            cb(dir, doneCb)
-            if (!didCallDone) done()
-        }),
-        withOptions: gen.withOptions,
-        withArguments: gen.withArguments,
-        withPrompts: gen.withPrompts,
-        withLocalConfig: gen.withLocalConfig,
-        withGenerators: gen.withGenerators,
-        run: () => new Promise((resolve, reject) => {
-            gen.on('ready', dir => { 
-                currentGenerator = {
-                    dir
-                } 
-            })
-            gen.on('end', resolve)
-            gen.on('error', reject)
+  let gen = helpers.run(genPath)
+
+  return {
+    setupDir: cb =>
+      gen.inTmpDir(function(dir) {
+        // Overengineered? Perhaps.
+        let done = this.async()
+        let didCallDone = false
+        let doneCb = () => {
+          if (didCallDone) return
+          didCallDone = true
+          done()
+        }
+        let ret = cb(dir, doneCb)
+        if (ret && 'then' in ret && typeof ret.then === 'function') {
+            ret.then(doneCb)
+        } else setTimeout(doneCb, 5000)
+      }),
+    withOptions: gen.withOptions,
+    withArguments: gen.withArguments,
+    withPrompts: gen.withPrompts,
+    withLocalConfig: gen.withLocalConfig,
+    withGenerators: gen.withGenerators,
+    run: () =>
+      new Promise((resolve, reject) => {
+        gen.on('ready', dir => {
+          tmpDir = dir.contextRoot
         })
-    }
+        gen.on('end', resolve)
+        gen.on('error', reject)
+      })
+  }
 }
 
 function cleanupGenerator() {
-    if (!currentGenerator) return
+  if (!tmpDir) return
 
-    return new Promise((resolve, reject) => {
-        rimraf(currentGenerator.dir, resolve)
-        currentGenerator = null
-    })
+  return new Promise(resolve => {
+    rimraf(tmpDir, resolve)
+    tmpDir = null
+  })
 }
 
 function file(filePath) {
-    if (!currentGenerator) return
+  if (!tmpDir) return
 
-    let actualFilePath = path.join(currentGenerator.dir, filePath)
-    if (fs.existsSync(actualFilePath)) return fs.readFileSync(actualFilePath)
+  let actualFilePath = path.join(tmpDir, filePath)
+  if (fs.existsSync(actualFilePath)) return fs.readFileSync(actualFilePath)
 }
 
 function fileJSON(filePath) {
-    let file = file(filePath)
-    if (file) return JSON.parse(file(filePath))
+  let file = file(filePath)
+  if (file) return JSON.parse(file(filePath))
 }
